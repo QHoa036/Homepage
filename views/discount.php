eeb4dbfeab1118d4942d7cec3bd802868def603e
ducntm <hoadinhtruong0306@gmail.com>
2024-10-09 16:36:51 +0700

    Finished discount

--- views/discount.php
+++ views/discount.php
@@ -8,6 +8,8 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/components/discount.css" />
+
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
 </head>
 
 <body>
@@ -62,14 +64,14 @@
         <div class="container-fluid bg-flashsale">
             <div class="row">
                 <div class="col-md-6 p-0">
-                    <div class="row-2 bg-flash-countdown">
-                        <img src="assets/imgs/buttons/btn_flashsale.png" alt="" />
-                        <p class="bg-flash-countdown-text">FLASH SALE: <span class="ms-2" id="countdown" /></p>
-                    </div>
+                    <div class="d-flex justify-content-center align-items-center mh-100 h-100 position-relative">
+                        <div class="bg-flash-countdown position-absolute top-0 start-0 ">
+                            <img src="assets/imgs/buttons/btn_flashsale.png" alt="" />
+                            <p class="bg-flash-countdown-text">FLASH SALE: <span class="ms-3" id="countdown" /></p>
+                        </div>
 
-                    <div class="row-10">
                         <div class="bg-flash-info shadow-sm border-4">
-                            <div class="d-flex justify-content-around align-items-center">
+                            <div class="d-flex justify-content-between align-items-center px-3">
                                 <h3 class="title">
                                     Máy Tính Casio FX-580VN X
                                 </h3>
@@ -80,37 +82,43 @@
                             </div>
 
                             <div class="bg-flash-info-body">
-                                <p class="content">Mã SP: <strong>SP5</strong></p>
+                                <p class="content">Mã SP: <strong class="ms-3">SP5</strong></p>
 
-                                <div class="color-info">
-                                    <div class="d-flex justify-content-around align-items-center">
-                                        <small class="content">Màu sắc: </small>
-
-                                        <span class="discount-color text-dark border-0 px-5 py-2 mx-4">Đen</span>
-                                        <span class="discount-color text-dark border-0 px-5 py-2">Hồng</span>
-                                    </div>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <p class="content me-2">Màu sắc: </p>
+                                    <?php
+                                        $colors = ['Đen', 'Hồng'];
+                                        foreach ($colors as $color) {
+                                            echo '<span
+                                                class="discount-color border-0 px-5 py-2 mx-4 d-flex
+                                                align-items-center justify-content-center">'
+                                                . htmlspecialchars($color) . '</span>';
+                                        }
+                                    ?>
                                 </div>
 
-                                <p class="price-info">Đã bán: <strong>500 sp</strong></p>
+                                <p class="content">Đã bán: <strong class="ms-3">500 sp</strong></p>
 
-                                <div class="d-flex justify-content-end align-items-center mb-2">
-                                    <div class="d-flex align-items-center mb-2">
-                                        <del class="text-muted me-2 price">350.000 đ</del>
-                                        <strong class="text-danger me-2 price">315.000 đ</strong>
+                                <div class="w-100 d-flex justify-content-end align-items-center px-4">
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <del class="text-muted me-2 discount-price">350.000 <u>đ</u></del>
+                                        <strong class="text-danger me-2 discount-price">315.000 <u>đ</u></strong>
                                     </div>
+
                                     <button class="btn btn-success btn-circle">
                                         <i class="fas fa-plus"></i>
                                     </button>
                                 </div>
 
-                                <div class="ratings">
-                                    <span>Đánh giá: 1 lượt</span>
-                                    <div class="stars">
+                                <div class="discount-ratings">
+                                    <p class="content">Đánh giá: <strong class="ms-3">1 lượt</strong></p>
+
+                                    <div class="discount-stars me-4">
                                         <i class="fas fa-star text-warning"></i>
                                         <i class="fas fa-star text-warning"></i>
                                         <i class="fas fa-star text-warning"></i>
                                         <i class="fas fa-star text-warning"></i>
-                                        <i class="far fa-star text-muted"></i>
+                                        <i class="far fa-star text-warning"></i>
                                     </div>
                                 </div>
                             </div>
@@ -119,15 +127,15 @@
                 </div>
 
                 <div class="col-md-6">
-                    <div class="owl-carousel owl-theme">
-                        <div class="item">
-                            <img src="assets/imgs/products/discount_1.png" alt="">
+                    <div class="flashsale-carousel owl-carousel owl-theme">
+                        <div class="item pb-3">
+                            <img src="assets/imgs/discounts/discount_1.png" alt="">
                         </div>
-                        <div class="item">
-                            <img src="assets/imgs/products/discount_2.png" alt="">
+                        <div class="item pb-3">
+                            <img src="assets/imgs/discounts/discount_2.png" alt="">
                         </div>
-                        <div class="item">
-                            <img src="assets/imgs/products/discount_3.png" alt="">
+                        <div class="item pb-3">
+                            <img src="assets/imgs/discounts/discount_3.png" alt="">
                         </div>
                     </div>
                 </div>
@@ -136,24 +144,49 @@
     </section>
 
     <!-- Banner Discount Section -->
-    <!-- <section class="BannerDiscount ">
-        <div class="container-fluid imgBannerDiscount">
-            <img src="assets/imgs/banners/discount.png" alt="" class="imgBannerDiscount">
+    <section class="banner-discount-section">
+        <div class="container-fluid">
+            <img src="assets/imgs/banners/bn_discount.png" alt="">
         </div>
-    </section> -->
+    </section>
 
     <!-- Voucher Section -->
-    <!-- <section class="voucher">
-        <div class="bgTitlevoucher">
-            <p class="titlevoucher">SĂN VOUCHER KHUYẾN MÃI</p>
+    <section class="banner-voucher-section">
+        <div class="bg-title-voucher">
+            <p class="title-voucher">SĂN VOUCHER KHUYẾN MÃI</p>
         </div>
-        <div class="container cardVoucher">
-            khuyễn mãi
+
+        <?php
+        $items = ['test', 'test'];
+        ?>
+
+        <div class="grid-container">
+            <?php foreach ($items as $item): ?>
+            <div class="voucher-card shadow-lg">
+                <div class="col-4">
+                    <img src="https://via.placeholder.com/150x150" alt="10% Off Discount" class="voucher-image">
+                </div>
+
+                <div class="col-8">
+                    <div class="row h-100 position-relative w-100">
+                        <div class="voucher-button position-absolute top-0 end-0">
+                            Lưu
+                        </div>
+
+                        <div class="voucher-title">Giảm 10%</div>
+                        <div class="voucher-details d-flex flex-column">
+                            <div class="text">
+                                Đơn Tối Thiểu 200.000đ <br />
+                                Có hiệu lực từ 30/10/2024 - 9/11/2024
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php endforeach; ?>
         </div>
-        <div class="container cardVoucher">
-            khuyễn mãi
-        </div>
-    </section> -->
+    </section>
 
     <script src="assets/scripts/discount.js"></script>
 </body>
